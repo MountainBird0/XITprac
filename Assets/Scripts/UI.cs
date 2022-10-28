9b7d2496405f090f033f3a9c72a698554b9fef16
Kim Jae Hwi <99602963+MountainBird0@users.noreply.github.com>
2022-10-28 14:19:29 +0900

    1028
    이전에 무언갈했었음

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public void OnClickEventButton()
+    {
+        Debug.Log("버튼누름");
+    }
+
+
+   
+}
